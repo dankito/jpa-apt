0ca2253a8d1244b030ef59e47011470f2feb9ee1
dankl <dankl@comfylight.com>
2017-06-13 15:47:45 +0200

    Checking roundEnv state, creating AnnotationProcessingContext and processing Entity configurations with EntityConfigurationReader

--- JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/JPAAnnotationProcessor.kt
+++ JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/JPAAnnotationProcessor.kt
@@ -1,9 +1,11 @@
 package net.dankito.jpa.apt
 
+import org.slf4j.LoggerFactory
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedAnnotationTypes
 import javax.lang.model.element.TypeElement
+import javax.tools.Diagnostic
 
 
 @SupportedAnnotationTypes("javax.persistence.*")
@@ -11,10 +13,28 @@ class JPAAnnotationProcessor : AbstractProcessor() {
 
     companion object {
         const val ALLOW_OTHER_PROCESSORS_TO_CLAIM_ANNOTATIONS: Boolean = true
+
+        private val log = LoggerFactory.getLogger(JPAAnnotationProcessor::class.java)
     }
 
 
     override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "Running " + javaClass.simpleName)
+
+        roundEnv?.let { roundEnv ->
+            if (roundEnv.processingOver() || annotations?.isEmpty() ?: true) {
+                return ALLOW_OTHER_PROCESSORS_TO_CLAIM_ANNOTATIONS
+            }
+
+            if (roundEnv.getRootElements() == null || roundEnv.getRootElements().isEmpty()) {
+                processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, "No sources to process")
+                return ALLOW_OTHER_PROCESSORS_TO_CLAIM_ANNOTATIONS
+            }
+
+            val context = AnnotationProcessingContext(roundEnv)
+
+            EntityConfigurationReader().readEntityConfigurations(context)
+        }
 
         return ALLOW_OTHER_PROCESSORS_TO_CLAIM_ANNOTATIONS
     }
