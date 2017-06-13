10619974daa331369d2af3e58aa6cebb2b061b1d
dankl <dankl@comfylight.com>
2017-06-13 15:46:46 +0200

    Started implementing EntityConfigurationReader

---
+++ JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/EntityConfigurationReader.kt
@@ -0,0 +1,162 @@
+package net.dankito.jpa.apt
+
+import net.dankito.jpa.apt.config.EntityConfig
+import java.lang.reflect.Method
+import java.sql.SQLException
+import javax.lang.model.element.Element
+import javax.lang.model.element.TypeElement
+import javax.persistence.*
+
+
+
+
+class EntityConfigurationReader {
+
+    fun readEntityConfigurations(context: AnnotationProcessingContext) {
+        for(entityClassElement in context.entityClasses) {
+            readEntityConfig(context, entityClassElement, listOf())
+        }
+    }
+
+
+    @Throws(SQLException::class)
+    private fun readEntityConfig(context: AnnotationProcessingContext, entityClassElement: Element, currentInheritanceTypeSubEntities: List<EntityConfig<*>>): EntityConfig<*> {
+        val asType = entityClassElement as TypeElement
+        val entityClass = Class.forName(asType.qualifiedName.toString())
+
+        var entityConfig = createEntityConfig(entityClass, currentInheritanceTypeSubEntities)
+
+        context.registerEntityConfig(entityConfig)
+
+        readEntityAnnotations(entityConfig, entityClassElement)
+        findLifeCycleEvents(entityConfig)
+
+        return entityConfig
+    }
+
+    @Throws(SQLException::class)
+    private fun <CLASS> createEntityConfig(entityClass: Class<CLASS>, currentInheritanceTypeSubEntities: List<EntityConfig<*>>): EntityConfig<CLASS> {
+        var entityConfig: EntityConfig<CLASS>? = null
+        val inheritanceStrategy = getInheritanceStrategyIfEntityIsInheritanceStartEntity(entityClass)
+
+        if (inheritanceStrategy == null) {
+            entityConfig = EntityConfig<CLASS>(entityClass)
+        }
+        else {
+//            entityConfig = createInheritanceEntityConfig(entityClass, inheritanceStrategy, currentInheritanceTypeSubEntities)
+            entityConfig = EntityConfig<CLASS>(entityClass)
+        }
+
+        entityConfig.tableName = entityConfig.entityClass.simpleName // default value, may overwritten by configuration in @Table or @Entity annotation
+
+        entityConfig.classHierarchy = getClassHierarchy(entityClass)
+
+        return entityConfig
+    }
+
+
+    @Throws(SQLException::class)
+    private fun readEntityAnnotations(entityConfig: EntityConfig<*>, entityClassElement: Element) {
+        readEntityAnnotation(entityConfig, entityClassElement)
+        readTableAnnotation(entityConfig, entityClassElement)
+        readAccessAnnotation(entityConfig, entityClassElement)
+    }
+
+    @Throws(SQLException::class)
+    private fun readEntityAnnotation(entityConfig: EntityConfig<*>, entityClassElement: Element) {
+        entityClassElement.getAnnotation(Entity::class.java)?.let { entityAnnotation ->
+            val name = entityAnnotation.name
+            if(name.isNullOrBlank() == false) {
+                entityConfig.tableName = name
+            }
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun readTableAnnotation(entityConfig: EntityConfig<*>, entityClassElement: Element) {
+        entityClassElement.getAnnotation(Table::class.java)?.let { tableAnnotation ->
+            entityConfig.tableName = tableAnnotation.name
+
+            entityConfig.catalogName = tableAnnotation.catalog
+            entityConfig.schemaName = tableAnnotation.schema
+            
+            entityConfig.uniqueConstraints = tableAnnotation.uniqueConstraints
+            entityConfig.indexes = tableAnnotation.indexes
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun readAccessAnnotation(entityConfig: EntityConfig<*>, entityClassElement: Element) {
+        entityClassElement.getAnnotation(Access::class.java)?.let { accessAnnotation ->
+            entityConfig.access = accessAnnotation.value
+        }
+    }
+
+
+    private fun findLifeCycleEvents(entityConfig: EntityConfig<*>) {
+        for(classWalk in entityConfig.classHierarchy) {
+            for(method in classWalk.declaredMethods) {
+                checkMethodForLifeCycleEvents(method, entityConfig)
+            }
+        }
+    }
+
+    private fun checkMethodForLifeCycleEvents(method: Method, entityConfig: EntityConfig<*>) {
+        //    List<Annotation> methodAnnotations = Arrays.asList(method.getAnnotations());
+
+        // TODO: i don't know what the specifications says but i implemented it this way that superclass life cycle events don't overwrite that ones from child classes
+        // (or should both be called?)
+        if (method.isAnnotationPresent(PrePersist::class.java))
+            entityConfig.addPrePersistLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PostPersist::class.java))
+            entityConfig.addPostPersistLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PostLoad::class.java))
+            entityConfig.addPostLoadLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PreUpdate::class.java))
+            entityConfig.addPreUpdateLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PostUpdate::class.java))
+            entityConfig.addPostUpdateLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PreRemove::class.java))
+            entityConfig.addPreRemoveLifeCycleMethod(method)
+        if (method.isAnnotationPresent(PostRemove::class.java))
+            entityConfig.addPostRemoveLifeCycleMethod(method)
+    }
+
+
+    private fun <CLASS> getInheritanceStrategyIfEntityIsInheritanceStartEntity(entityClass: Class<CLASS>): InheritanceType? {
+
+        return null
+    }
+
+
+    private fun getClassHierarchy(entityClass: Class<*>) : List<Class<*>> {
+        val classHierarchy = mutableListOf<Class<*>>()
+        var classWalk: Class<*>? = entityClass
+
+        while (classWalk != null) {
+            if (classIsEntityOrMappedSuperclass(classWalk)) {
+                classHierarchy.add(classWalk)
+
+                classWalk = classWalk.javaClass.superclass
+            }
+            else {
+                classWalk = null
+            }
+        }
+
+        return classHierarchy
+    }
+
+    private fun classIsEntityOrMappedSuperclass(entityClass: Class<*>): Boolean {
+        return classIsEntity(entityClass) || classIsMappedSuperClass(entityClass)
+    }
+
+    private fun classIsEntity(entityClass: Class<*>): Boolean {
+        return entityClass.isAnnotationPresent(Entity::class.java)
+    }
+
+    private fun classIsMappedSuperClass(entityClass: Class<*>): Boolean {
+        return entityClass.isAnnotationPresent(MappedSuperclass::class.java)
+    }
+
+}
