da63ef96be4b6c1b50f7cba9b41e980b1ce99b77
dankl <dankl@comfylight.com>
2017-06-14 13:18:23 +0200

    Added getType()

--- JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/config/Property.kt
+++ JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/config/Property.kt
@@ -4,4 +4,22 @@ import java.lang.reflect.Field
 import java.lang.reflect.Method
 
 
-data class Property(val field: Field, val getter: Method?, val setter: Method?)
+data class Property(val field: Field, val getter: Method?, val setter: Method?) {
+
+    fun getType(): Class<*> {
+        if (field != null) {
+            return field.type
+        }
+
+        if (getter != null) {
+            return getter.getReturnType()
+        }
+
+        if (setter != null && setter.getParameterTypes().size == 1) {
+            return setter.getParameterTypes()[0]
+        }
+
+        throw RuntimeException("Could not get type for property $this") // should never come to this
+    }
+
+}
