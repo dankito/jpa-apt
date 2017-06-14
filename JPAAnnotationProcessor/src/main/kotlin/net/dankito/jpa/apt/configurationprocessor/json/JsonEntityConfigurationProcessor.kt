c412ce52fa52da9d6b640e1d578a1c0fdcbd8d9b
dankl <dankl@comfylight.com>
2017-06-15 00:43:19 +0200

    Implemented writing serializedConfiguration to a Java source file

--- JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/configurationprocessor/json/JsonEntityConfigurationProcessor.kt
+++ JPAAnnotationProcessor/src/main/kotlin/net/dankito/jpa/apt/configurationprocessor/json/JsonEntityConfigurationProcessor.kt
@@ -7,9 +7,11 @@ import net.dankito.jpa.apt.config.JpaEntityConfiguration
 import net.dankito.jpa.apt.configurationprocessor.IEntityConfigurationProcessor
 import net.dankito.jpa.apt.configurationprocessor.json.serializer.*
 import org.slf4j.LoggerFactory
+import java.io.OutputStreamWriter
 import java.lang.reflect.Constructor
 import java.lang.reflect.Field
 import java.lang.reflect.Method
+import javax.annotation.processing.ProcessingEnvironment
 
 
 class JsonEntityConfigurationProcessor : IEntityConfigurationProcessor {
@@ -43,16 +45,30 @@ class JsonEntityConfigurationProcessor : IEntityConfigurationProcessor {
     }
 
 
-    override fun processConfiguration(entityConfiguration: JpaEntityConfiguration) {
+    override fun processConfiguration(entityConfiguration: JpaEntityConfiguration, processingEnv: ProcessingEnvironment) {
         try {
             val serializedConfiguration = objectMapper.writeValueAsString(entityConfiguration)
 
-            val deserializedConfiguration = objectMapper.readValue(serializedConfiguration, JpaEntityConfiguration::class.java)
-            if(deserializedConfiguration != null) {
-
-            }
+            writeSerializedConfigurationToFile(serializedConfiguration, processingEnv)
         } catch(e: Exception) {
             log.error("Could not serialize EntityConfiguration", e)
         }
     }
+
+    private fun writeSerializedConfigurationToFile(serializedConfiguration: String, processingEnv: ProcessingEnvironment) {
+        val packageName = "net.dankito.data_access.database"
+        val file = processingEnv.filer.createSourceFile(packageName + ".GeneratedModel")
+
+        val writer = OutputStreamWriter(file.openOutputStream(), "utf-8")
+
+        val classCode = "package " + packageName + ";" + System.lineSeparator() + System.lineSeparator() +
+                "public class GeneratedModel {" + System.lineSeparator() +
+                "    public static final String JSON = \"" + serializedConfiguration.replace("\"", "\\\"") + "\";" + System.lineSeparator() +
+                "}"
+        writer.write(classCode)
+
+        writer.flush()
+        writer.close()
+    }
+
 }
