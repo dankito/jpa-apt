e2081fb4eacdffc1c1df278393617b1d5f2c2714
dankl <dankl@comfylight.com>
2017-06-16 12:47:24 +0200

    Removed files which should have never been checked in

---
+++ JPAAnnotationProcessor/target/output/net/dankito/jpa/apt/test_entities/inheritance_annotations/Child_1_2EntityConfig.java
@@ -0,0 +1,6 @@
+package net.dankito.jpa.apt.test_entities.inheritance_annotations;
+
+public final class Child_1_2EntityConfig {
+  public Child_1_2EntityConfig() {
+  }
+}
---
+++ JPAAnnotationProcessor/target/output/net/dankito/jpa/apt/test_entities/inheritance_annotations/EntityWithoutSuperclassEntityConfig.java
@@ -0,0 +1,6 @@
+package net.dankito.jpa.apt.test_entities.inheritance_annotations;
+
+public final class EntityWithoutSuperclassEntityConfig {
+  public EntityWithoutSuperclassEntityConfig() {
+  }
+}
