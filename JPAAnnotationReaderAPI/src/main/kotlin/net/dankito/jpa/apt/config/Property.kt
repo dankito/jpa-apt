68852c64715fa22b2464b79363b09cdd1a3be14d
dankl <dankl@comfylight.com>
2017-06-17 13:38:54 +0200

    Added convenience method getGenericType()

--- JPAAnnotationReaderAPI/src/main/kotlin/net/dankito/jpa/apt/config/Property.kt
+++ JPAAnnotationReaderAPI/src/main/kotlin/net/dankito/jpa/apt/config/Property.kt
@@ -2,6 +2,8 @@ package net.dankito.jpa.apt.config
 
 import java.lang.reflect.Field
 import java.lang.reflect.Method
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 
 
 data class Property(val field: Field, val getter: Method?, val setter: Method?) {
@@ -23,6 +25,26 @@ data class Property(val field: Field, val getter: Method?, val setter: Method?) 
         }
 
         throw RuntimeException("Could not get type for property $this") // should never come to this
+    }
+
+    fun getGenericType(): Class<*>? {
+        if (field.genericType is ParameterizedType) {
+            return getGenericTypeClassFromType(field.genericType)
+        }
+
+        if (getter != null && getter.genericReturnType is ParameterizedType) {
+            return getGenericTypeClassFromType(getter.genericReturnType)
+        }
+
+        if (setter != null && setter.parameterTypes.size == 1 && setter.getGenericParameterTypes()[0] is ParameterizedType) {
+            return getGenericTypeClassFromType(setter.genericParameterTypes[0])
+        }
+
+        return null
+    }
+
+    private fun getGenericTypeClassFromType(genericType: Type): Class<*> {
+        return (genericType as ParameterizedType).actualTypeArguments[0] as Class<*>
     }
 
 }
